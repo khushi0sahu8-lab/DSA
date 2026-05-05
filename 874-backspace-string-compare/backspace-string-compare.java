73a8691ff3a654f65e2081528eae6aaf0c427e12
khushi0sahu8-lab <khushi0sahu8@gmail.com>
2026-05-05 11:22:18 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (51.51%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        while (i >= 0 || j >= 0) {
+            i = getNextValidIndex(s, i);
+            j = getNextValidIndex(t, j);
+
+            if (i < 0 && j < 0) return true;
+            if (i < 0 || j < 0) return false;
+
+            if (s.charAt(i) != t.charAt(j)) return false;
+
+            i--;
+            j--;
+        }
+        return true;
+    }
+
+    private int getNextValidIndex(String str, int i) {
+        int skip = 0;
+
+        while (i >= 0) {
+            if (str.charAt(i) == '#') {
+                skip++;
+                i--;
+            } else if (skip > 0) {
+                skip--;
+                i--;
+            } else {
+                break;
+            }
+        }
+        return i;
+    }
+}
