9f28c722a9a24fac0a463afdc5bcf80a32410db1
khushi0sahu8-lab <khushi0sahu8@gmail.com>
2026-05-06 22:53:05 +0530

    Time: 1 ms (2.32%) | Memory: 43.9 MB (61.10%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,18 +1,43 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        if (m == 0) return false;
-        int n = matrix[0].length;
-
-        int left = 0, right = m * n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int i = mid / n, j = mid % n;
-            if(matrix[i][j] == target) return true;
-            else if(target < matrix[i][j]) right = mid - 1;
-            else left = mid + 1;
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int low=matrix[0][0];
+        int high=0;
+        for(int []row :matrix){
+            for(int num :row){
+                high=Math.max(num,high);
+            }
+            
+            while(low<=high){
+                int mid=low+(high-low)/2;
+                boolean res=checkmid(matrix,mid);
+                if(res &&mid==target){
+                    return true;
+                }
+                else if(mid<target){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
         }
-
         return false;
+        
+    }
+    public static boolean checkmid(int[][] matrix,int mid){
+        
+        for(int []row : matrix){
+            for(int num : row){
+                if(num==mid){
+                   return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String []args){
+        int [][]matrix={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        boolean res=searchMatrix(matrix,3);
+        System.out.println(res);
     }
 }
