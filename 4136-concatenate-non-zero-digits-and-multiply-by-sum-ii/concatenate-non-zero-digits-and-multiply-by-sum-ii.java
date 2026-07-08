960208e55a939791cf9a6816dbf85aeff768bbd1
khushi0sahu8-lab <khushi0sahu8@gmail.com>
2026-07-08 14:33:02 +0530

    Time: 14 ms (100.00%) | Memory: 131.3 MB (23.33%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -1,95 +1,44 @@
 class Solution {
-    static final long MOD = 1_000_000_007L;
-
-    class Node {
-        long num;
-        long sum;
-        int len;
-
-        Node(long num, long sum, int len) {
-            this.num = num;
-            this.sum = sum;
-            this.len = len;
-        }
-    }
-
-    Node[] tree;
-    long[] pow10;
-
     public int[] sumAndMultiply(String s, int[][] queries) {
-        int n = s.length();
-
-        pow10 = new long[n + 1];
-        pow10[0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            pow10[i] = (pow10[i - 1] * 10) % MOD;
-        }
-
-        tree = new Node[4 * n];
-        build(1, 0, n - 1, s);
-
-        int[] ans = new int[queries.length];
-
-        for (int i = 0; i < queries.length; i++) {
-            Node res = query(1, 0, n - 1,
-                    queries[i][0], queries[i][1]);
-
-            ans[i] = (int)((res.num * res.sum) % MOD);
-        }
-
-        return ans;
-    }
-
-
-    void build(int idx, int l, int r, String s) {
-        if (l == r) {
-            int d = s.charAt(l) - '0';
-
-            if (d == 0) {
-                tree[idx] = new Node(0, 0, 0);
+        long MOD = 1_000_000_007;
+        int len = s.length();
+        
+        long[] preSum = new long[len + 1];
+        long[] preProduct = new long[len + 1];
+        int[] nonZeroCnt = new int[len + 1];
+        long[] p10 = new long[len + 1];
+ 
+        p10[0] = 1;
+        for (int i = 0; i < len; i++) {
+            p10[i + 1] = (p10[i] * 10) % MOD;
+            
+            int digit = s.charAt(i) - '0';
+            preSum[i + 1] = preSum[i] + digit;
+            
+            if (digit == 0) {
+                preProduct[i + 1] = preProduct[i];
+                nonZeroCnt[i + 1] = nonZeroCnt[i];
             } else {
-                tree[idx] = new Node(d, d, 1);
+                preProduct[i + 1] = (preProduct[i] * 10 + digit) % MOD;
+                nonZeroCnt[i + 1] = nonZeroCnt[i] + 1;
             }
-            return;
         }
+        
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
 
-        int mid = (l + r) / 2;
+            long sum = preSum[end + 1] - preSum[start];
 
-        build(idx * 2, l, mid, s);
-        build(idx * 2 + 1, mid + 1, r, s);
+            int cnt = nonZeroCnt[end + 1] - nonZeroCnt[start];
 
-        tree[idx] = merge(tree[idx * 2], tree[idx * 2 + 1]);
-    }
-
-
-    Node merge(Node a, Node b) {
-
-        long newNum = (a.num * pow10[b.len] + b.num) % MOD;
-
-        long newSum = a.sum + b.sum;
-
-        int newLen = a.len + b.len;
-
-        return new Node(newNum, newSum, newLen);
-    }
-
-
-    Node query(int idx, int l, int r, int ql, int qr) {
-
-        if (qr < l || r < ql) {
-            return new Node(0, 0, 0);
+            long subtract = (preProduct[start] * p10[cnt]) % MOD;
+            long x = (preProduct[end + 1] - subtract + MOD) % MOD;
+            
+            res[i] = (int) ((x * sum) % MOD);
         }
-
-        if (ql <= l && r <= qr) {
-            return tree[idx];
-        }
-
-        int mid = (l + r) / 2;
-
-        Node left = query(idx * 2, l, mid, ql, qr);
-        Node right = query(idx * 2 + 1, mid + 1, r, ql, qr);
-
-        return merge(left, right);
+        
+        return res;
     }
 }
